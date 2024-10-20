f52e5088a47a1e331dc0cb5671f0641515a42816
kulizhnikovjeka <jekavorkuta@mail.ru>
2024-10-20 20:32:57 +0300

    feat: реализована логики создания и удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
-// @todo: Темплейт карточки
+// ui-elements
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template');
 
-// @todo: DOM узлы
+// functions
+const createPlaceCard = (cardData, deleteCard) => {
+  const card = cardTemplate.content.cloneNode(true);
 
-// @todo: Функция создания карточки
+  const img = card.querySelector('.card__image');
+  img.src = cardData?.link;
+  img.alt = cardData?.name;
 
-// @todo: Функция удаления карточки
+  const title = card.querySelector('.card__title');
+  title.textContent = cardData?.name;
 
-// @todo: Вывести карточки на страницу
+  const deleteButton = card.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', deleteCard);
+
+  return card;
+}
+
+const deletePlaceCard = (event) => {
+  const card = event.target.parentNode;
+  card.remove();
+}
+
+const renderPlaceCards = () => {
+  initialCards.forEach(cardData => {
+    const card = createPlaceCard(cardData, deletePlaceCard);
+    placesList.append(card)
+  });
+}
+
+renderPlaceCards();
